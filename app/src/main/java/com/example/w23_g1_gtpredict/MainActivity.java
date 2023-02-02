4427e1c57d8717238c606b295f51d638dec2b9a6
Pamuditha Tissakuttiarachchi <tissakuttiaracp@student.douglascollege.ca>
2023-02-02 14:44:16 -0800

    Initial commit

---
+++ app/src/main/java/com/example/w23_g1_gtpredict/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.w23_g1_gtpredict;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
