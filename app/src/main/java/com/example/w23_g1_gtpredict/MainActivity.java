b3d9ffe99d1e32d217c539970ad4423f2edcac3a
abulazad20 <chandbatchaa@student.douglascollege.ca>
2023-02-14 19:00:56 -0800

    commit contribution update

--- app/src/main/java/com/example/w23_g1_gtpredict/MainActivity.java
+++ app/src/main/java/com/example/w23_g1_gtpredict/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.w23_g1_gtpredict;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//  test gihub update
 public class MainActivity extends AppCompatActivity {
 
     @Override
