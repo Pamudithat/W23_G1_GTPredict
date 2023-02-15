59afbfa47454f8d55a417346ba902fd0c2e26d0d
abulazad20 <chandbatchaa@student.douglascollege.ca>
2023-02-14 19:05:34 -0800

    Adding prototype developed during feb 1st week for proposal work

--- app/src/main/java/com/example/w23_g1_gtpredict/MainActivity.java
+++ app/src/main/java/com/example/w23_g1_gtpredict/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.w23_g1_gtpredict;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-//  test gihub update
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 public class MainActivity extends AppCompatActivity {
+Button btnEnter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEnter = findViewById(R.id.gtPredictEnterbtn);
+        btnEnter.setOnClickListener((View view) ->{
+            startActivity(new Intent(MainActivity.this,QuickPredActivity.class));
+
+        });
     }
 }
+
